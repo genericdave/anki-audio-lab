1589e679582971af6e19a4345f61874cc62a8bb7
Genericdave <168910+genericdave@users.noreply.github.com>
2024-01-06 23:40:12 -0500

    logging and double quotes

--- src/index.js
+++ src/index.js
@@ -1,42 +1,44 @@
-import _ from 'lodash';
-import WaveSurfer from 'wavesurfer.js';
+import _ from "lodash";
+import WaveSurfer from "wavesurfer.js";
 import { ankiConnectInvoke } from "./util.js";
 
 
 // Init
 const WS = WaveSurfer.create({
-    container: '#waveform',
-    waveColor: 'rgba(200, 200, 200, 0.5)',
-    progressColor: 'rgba(100, 100, 100, 0.5)',
+    container: "#waveform",
+    waveColor: "rgba(200, 200, 200, 0.5)",
+    progressColor: "rgba(100, 100, 100, 0.5)",
     minPxPerSec: 200,
     sampleRate: 11025,
 });
 
 
 // Elements
-const StatusText = document.getElementById('status');
-const FieldNameSelect = document.getElementById('field-name-select');
-const RegexPatternInput = document.getElementById('regex-pattern-input');
-const CardFieldsElement = document.getElementById('card-fields');
+const StatusText = document.getElementById("status");
+const FieldNameSelect = document.getElementById("field-name-select");
+const RegexPatternInput = document.getElementById("regex-pattern-input");
+const CardFieldsElement = document.getElementById("card-fields");
 
 
 // Events
-FieldNameSelect.addEventListener('change', displayCurrentCard);
-RegexPatternInput.addEventListener('input', displayCurrentCard);
-WS.on('interaction', () => {
+FieldNameSelect.addEventListener("change", displayCurrentCard);
+RegexPatternInput.addEventListener("input", displayCurrentCard);
+WS.on("interaction", () => {
     WS.playPause();
 });
 
 
 // Logic
 function clearAudio() {
+    console.log("audio cleared");
     WS.empty();
     WS.toggleInteraction(false);
 }
 
 function clearCardInfo() {
-    FieldNameSelect.innerHTML = '';
-    CardFieldsElement.textContent = '';
+    console.log("card cleared");
+    FieldNameSelect.innerHTML = "";
+    CardFieldsElement.textContent = "";
 }
 
 function audioError(e) {
@@ -52,20 +54,20 @@ function cardError(e) {
 
 async function retrieveAudio(filename) {
     try {
-        const result = await ankiConnectInvoke('retrieveMediaFile', 6, { filename });
+        const result = await ankiConnectInvoke("retrieveMediaFile", 6, { filename });
 
         if (!result) {
-            updateStatus('Audio file not found');
+            updateStatus("Audio file not found");
             return;
         }
 
         const audioBlob = atob(result); // Decode base64
         const audioBuffer = new Uint8Array(audioBlob.length).map((_, i) => audioBlob.charCodeAt(i));
-        const audioUrl = URL.createObjectURL(new Blob([audioBuffer], { type: 'audio/mp3' }));
+        const audioUrl = URL.createObjectURL(new Blob([audioBuffer], { type: "audio/mp3" }));
 
         WS.load(audioUrl);
         WS.toggleInteraction(true);
-        updateStatus('Audio file loaded');
+        updateStatus("Audio file loaded");
     } catch (e) {
         audioError(e);
     }
@@ -76,7 +78,7 @@ function displayCurrentCard() {
     CardFieldsElement.textContent = JSON.stringify(CurrentCard.fields, null, 4);
 
     const fieldName = FieldNameSelect.value;
-    const fieldValue = _.get(CurrentCard, ['fields', fieldName, 'value']);
+    const fieldValue = _.get(CurrentCard, ["fields", fieldName, "value"]);
 
     try {
         const matches = fieldValue.match(RegExp(RegexPatternInput.value));
@@ -100,7 +102,8 @@ function populateFieldNames() {
 
     let optionsHTML = "";
     for (const field of Object.keys(CurrentCard.fields)) {
-        optionsHTML += `<option value="${field}" ${field === previousSelection ? 'selected' : ''}}>${field}</option>`;
+        if (field === previousSelection) { console.log(`Reselecting "${previousSelection}" field`); }
+        optionsHTML += `<option value="${field}" ${field === previousSelection ? "selected" : ""}}>${field}</option>`;
     }
 
     FieldNameSelect.innerHTML = optionsHTML;
@@ -109,7 +112,7 @@ function populateFieldNames() {
 let CurrentCard = { "cardId": null };
 async function fetchCurrentCard() {
     try {
-        const newCard = await ankiConnectInvoke('guiCurrentCard', 6);
+        const newCard = await ankiConnectInvoke("guiCurrentCard", 6);
         if (!newCard || newCard.cardId === CurrentCard.cardId) return;
         CurrentCard = newCard;
         updateStatus(`Card with ID ${CurrentCard.cardId} fetched`);
